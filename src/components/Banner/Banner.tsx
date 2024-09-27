37dac7d08634250b34fcb2f211a3ac1ff55ba4fe
BijanProgrammer <bijaneisapour@gmail.com>
2024-09-27 19:20:10 +0330

    [FIX] wrong image path

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -8,14 +8,14 @@ function Banner() {
         isPrimary={true}
         title="خدمات پزشکی رو برای شما در دسترستر کردیم"
         description="فقط با چند کلیک به خدمات پزشکی مورد نیاز افراد کم توان جسمی(حرکتی)دسترسی پیدا میکنید."
-        img="images/Banner/pic1.png"
+        img="images/banner/pic1.png"
         btnText="مشاهده خدمات"
       />
       <BannerCard
         isPrimary={false}
         title="مشغله زندگی نمیزاره بیایی دکتر؟ مشاوره غیر حضوری بگیر"
         description="اگر زمان کافی برای مراجعه به پزشک رو ندارید میتونید از طریق مشاوره غیر حضوری با پزشک صحبت کنید"
-        img="images/Banner/pic2.png"
+        img="images/banner/pic2.png"
         btnText="نوبت بگیر"
       />
     </div>
