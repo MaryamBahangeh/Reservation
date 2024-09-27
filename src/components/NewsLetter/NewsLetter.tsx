a21778e6ba43c4a4498df6905d24e1d9a844ca69
BijanProgrammer <bijaneisapour@gmail.com>
2024-09-27 20:42:24 +0330

    [FIX] image names

--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -5,14 +5,14 @@ function NewsLetter(props) {
   return (
     <div className={styles["news-letter"]}>
       <div className={styles.imagediv}>
-        <img src="images/NewsLetter/image.png" alt="newsletter" />
+        <img src="/images/news-letter/image.png" alt="newsletter" />
       </div>
       <p>بیمه های طرف قرارداد نوبیتو</p>
-      <img src="images/NewsLetter/Pic.png" alt="newsletter" />
-      <img src="images/NewsLetter/Pic1.png" alt="newsletter" />
-      <img src="images/NewsLetter/Pic2.png" alt="newsletter" />
-      <img src="images/NewsLetter/Pic3.png" alt="newsletter" />
-      <img src="images/NewsLetter/Pic4.png" alt="newsletter" />
+      <img src="/images/news-letter/pic.png" alt="newsletter" />
+      <img src="/images/news-letter/pic1.png" alt="newsletter" />
+      <img src="/images/news-letter/pic2.png" alt="newsletter" />
+      <img src="/images/news-letter/pic3.png" alt="newsletter" />
+      <img src="/images/news-letter/pic4.png" alt="newsletter" />
     </div>
   );
 }
