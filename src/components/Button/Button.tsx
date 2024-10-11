90bd278d9de21030b2ca55cb0cb6bb7581ddf54a
BijanProgrammer <bijaneisapour@gmail.com>
2024-10-11 20:25:47 +0330

    [UPDATE] button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,19 @@
 import styles from "./Button.module.css";
-import { PropsWithChildren } from "react";
+import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+
 export enum Variant {
   FILL = "fill",
   OUTLINE = "outline",
   TEXT = "text",
 }
+
 export enum Color {
   PRIMARY = "primary",
   SECONDARY = "secondary",
   BLACK = "black",
   WHITE = "white",
 }
+
 export enum Size {
   SMALL = "small",
   MEDIUM = "medium",
@@ -18,15 +21,20 @@ export enum Size {
   HUGE = "huge",
 }
 
-type props = PropsWithChildren & {
-  variant: Variant;
-  color: Color;
-  size: Size;
-};
-function Button({ variant, color, size, children }: props) {
+type HtmlButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+type Props = HtmlButtonProps &
+  PropsWithChildren & {
+    variant: Variant;
+    color: Color;
+    size: Size;
+  };
+
+function Button({ variant, color, size, children, ...rest }: Props) {
   return (
     <button
       className={`${styles.button} ${styles[variant]} ${styles[color]} ${styles[size]}`}
+      {...rest}
     >
       {children}
     </button>
